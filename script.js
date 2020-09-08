a2e7e469bcea6bfbcc269d63cf8318e5b03f746a
Diego Do Santos <ddosantos@hotmail.com>
2020-09-08 19:38:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const AUTOS = 'https://ferqueve.github.io/autorizo/automoviles.json';
+const PROPIETARIOS = 'https://ferqueve.github.io/autorizo/propietarios.json';
+
+var getJSONData = function (url) {
+	var result = {};
+	return fetch(url)
+		.then((response) => {
+			if (response.ok) {
+				return response.json();
+			} else {
+				throw Error(response.statusText);
+			}
+		})
+		.then(function (response) {
+			result.status = "ok";
+			result.data = response;
+			return result;
+		})
+		.catch(function (error) {
+			result.status = "error";
+			result.data = error;
+			return result;
+		});
+};
+
+document.addEventListener("DOMContentLoaded", function (e) {
+
+	const contenidoDOM = document.getElementById("contenido");
+
+	getJSONData(PROPIETARIOS).then(function (resultObj) {
+		if (resultObj.status === "ok") {
+			let propietarios = resultObj.data;
+
+			getJSONData(AUTOS).then(function (resultObj) {
+				if (resultObj.status === "ok") {
+					let autos = resultObj.data;
+
+					for (let propietario of propietarios) {
+						contenidoDOM.innerHTML += `<br><b>${propietario.nombre}</b>`
+						for (let auto of autos) {
+							if (propietario.id === auto.idprop) {
+								contenidoDOM.innerHTML += `<br><code>${auto.marca} ${auto.modelo} ${auto.anio}</code>`
+							}
+						}
+					};
+				}
+			});
+		}
+	});
+});
